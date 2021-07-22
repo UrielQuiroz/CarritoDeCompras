db359fd4806b9ac7a93100ce586038568efcf47b
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-07-22 11:02:42 -0500

    Leer los datos del curso seleccionado
    Se creo la función LeerDatosCurso()

--- js/app.js
+++ js/app.js
@@ -14,13 +14,31 @@
     cargarEventListeners();
 
     function cargarEventListeners() {
+        //Cuando agregas un curso presionando "Agregar al carrito"
         listaCursos.addEventListener('click', agregarCurso);
     }
 
     function agregarCurso(e) {
         e.preventDefault();
         //console.log(e.target.classList)
+
         if(e.target.classList.contains('agregar-carrito')){
-            console.log('Agregando al carrito');
+            //console.log('Agregando al carrito');
+            const cursoSeleccionado = e.target.parentElement.parentElement;
+            leerDatosCurso(cursoSeleccionado);
         }
+
+    }
+
+    function leerDatosCurso(curso) {
+        console.log(curso);
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            titulo: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('.precio span').textContent,
+            id: curso.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+
+        console.log(infoCurso);
     }
